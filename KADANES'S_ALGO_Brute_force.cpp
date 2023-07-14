c99c3690f37ab95905f1abf45c7e3038a3a6a453
VrrGo <136469991+VrrGo@users.noreply.github.com>
2023-07-14 13:50:19 +0530

    Update KADANES'S_ALGO_Brute_force.cpp

--- KADANES'S_ALGO_Brute_force.cpp
+++ KADANES'S_ALGO_Brute_force.cpp
@@ -1,11 +1,9 @@
 class Solution{
     public:
-    // arr: input array
-    // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
+   
     long long maxSubarraySum(int arr[], int n){
         
-        // Your code here
+       
         long long max_sum=arr[0];
         for(int i=0;i<n;i++) {
             long long sum=0;
